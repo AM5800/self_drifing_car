b7e63416ae0a215b3eec0da87c72a2c60a1d8de2
AM5800 <am5800@yandex.ru>
2017-01-12 20:38:12 +0100

    Alexnet, VGG and grid search implemented

--- behavioral_cloning/dataset.py
+++ behavioral_cloning/dataset.py
@@ -1,6 +1,6 @@
+import cv2
 import numpy as np
-import pickle
-import os
+
 
 def convert_dataset(convert_fun, dataset):
     return np.array(list(map(convert_fun, dataset)))
@@ -8,49 +8,59 @@ def convert_dataset(convert_fun, dataset):
 
 class Dataset:
     def __init__(self, x_train, y_train, x_validation, y_validation, x_test, y_test):
-        self.x_train = x_train
+        self.X_train = x_train
         self.y_train = y_train
-        self.x_validation = x_validation
+        self.X_validation = x_validation
         self.y_validation = y_validation
-        self.x_test = x_test
+        self.X_test = x_test
         self.y_test = y_test
 
 
 class DatasetProvider:
-    __datasets = {}
-    __converters = {}
+    def __init__(self):
+        __dataset = None
+        self.__datasets = {}
+        self.__converters = {}
 
-    def __init__(self, create_dataset_function):
-        file_name = "dataset.p"
-
-        if os.path.exists(file_name):
-            self.dataset = pickle.load(open(file_name, "rb"))
-        else:
-            self.dataset = create_dataset_function()
-            pickle.dump(self.dataset, open(file_name, "wb"))
-
-        self.register("original", lambda x: x)
+    def initialize(self, dataset):
+        self.__dataset = dataset
 
     def register(self, name, conversion_function):
-        origin = self.dataset
-        x_train = convert_dataset(conversion_function, origin.x_train)
-        x_valid = convert_dataset(conversion_function, origin.x_validation)
-        x_test = convert_dataset(conversion_function, origin.x_test)
-
-        dataset = Dataset(x_train, origin.y_train, x_valid, origin.y_validation, x_test, origin.y_test)
         self.__converters[name] = conversion_function
-        self.__datasets[name] = dataset
 
     def get(self, name):
-        return self.__datasets[name]
+        if name in self.__datasets:
+            return self.__datasets[name]
+
+        conversion_function = self.__converters[name]
+        origin = self.__dataset
+        X_train = convert_dataset(conversion_function, origin.X_train)
+        X_valid = convert_dataset(conversion_function, origin.X_validation)
+        X_test = convert_dataset(conversion_function, origin.X_test)
+
+        dataset = Dataset(X_train, origin.y_train, X_valid, origin.y_validation, X_test, origin.y_test)
+        self.__datasets[name] = dataset
+
+        return dataset
 
     def convert(self, name, data):
         converter = self.__converters[name]
         return convert_dataset(converter, data)
 
     def get_shape(self, name):
-        return self.__datasets[name].x_train[0].shape
+        self.get(name)
+        return self.__datasets[name].X_train[0].shape
 
     def get_dataset_names(self):
         return self.__datasets.keys()
 
+
+dataset_provider = DatasetProvider()
+
+
+def to_hsv(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+
+dataset_provider.register("hsv", to_hsv)
+dataset_provider.register("original", lambda x: x)
--- behavioral_cloning/model.py
+++ behavioral_cloning/model.py
@@ -1,66 +1,255 @@
 import csv
-
-import matplotlib.image as mpimg
 import numpy as np
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization, Activation
 from keras.models import Sequential
+import os
+import matplotlib.image as mpimg
+import copy
+from keras import backend as kb
+import time
+from dataset import dataset_provider, Dataset
 
-import dataset
-import utils
 
-
-def load_dataset(log_file="driving_log.csv"):
+# Reads driving log and loads center image + steering angle
+def read_log_file(file_path):
     xs = []
     ys = []
-
-    with open(log_file) as csvfile:
+    with open(file_path) as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             img_path = row[0]
-            steering_angle = row[3]
+
+            if not os.path.isfile(img_path):
+                continue
+
+            steering_angle = float(row[3])
 
             img = mpimg.imread(img_path)
-
             xs.append(img)
             ys.append(steering_angle)
 
     xs = np.array(xs)
     ys = np.array(ys)
 
-    return utils.shuffle_split_to_dataset(xs, ys, 0.6, 0.2)
+    return xs, ys
 
 
-dataset_provider = dataset.DatasetProvider(load_dataset)
+def load_dataset():
+    # Train set and validation set both were created manually
+    # and held separately
+    X_train, y_train = read_log_file("train/driving_log.csv")
+    X_valid, y_valid = read_log_file("validation/driving_log.csv")
 
-input_shape = dataset_provider.get_shape("original")
-dataset = dataset_provider.get("original")
-
-print("Trainset shape: ", dataset.x_train.shape)
-
-model = Sequential([
-    Conv2D(64, 3, 3, activation="relu", input_shape=input_shape),
-    MaxPooling2D(),
-    Conv2D(128, 3, 3, activation="relu"),
-    MaxPooling2D(),
-    Flatten(),
-    Dense(1)
-])
+    return Dataset(X_train, y_train, X_valid, y_valid, [], [])
 
 
-for l in model.layers:
-    print(l.name, l.input_shape, l.output_shape)
-
-model.compile(loss='mse',
-              optimizer='adam')
-
-history = model.fit(dataset.x_train, dataset.y_train,
-                    batch_size=50, nb_epoch=10,
-                    verbose=2, validation_data=(dataset.x_validation, dataset.y_validation))
+dataset_provider.initialize(load_dataset())
 
 
-json = model.to_json()
-with open("model.json", "w") as file:
-    file.write(json)
+def save_model(model, dataset_name, name):
+    json = model.to_json()
+    with open(name + ".json", "w") as file:
+        file.write(json)
 
-model.save_weights("model.h5")
+    model.save_weights(name + ".h5")
+    with open(name + ".cfg", "w") as cfg:
+        cfg.write(dataset_name)
 
+
+# Defines AlexNet-like network
+# With 4 Convolution layers (Conv2d -> BN -> relu -> MaxPool)
+# And 2 Fully-connected layers
+def alexnet(input_shape, dropout, use_bn):
+    model = Sequential()
+    model.add(Conv2D(32, 3, 3, input_shape=input_shape))
+
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(64, 3, 3))
+
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(128, 3, 3))
+
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(256, 3, 3))
+
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D())
+
+    model.add(Flatten())
+
+    model.add(Dense(1000))
+
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Activation("relu"))
+
+    model.add(Dropout(dropout))
+
+    model.add(Dense(1))
+
+    return model
+
+
+# Defines VGG-like network
+# With 9 Convolution layers (Conv2d -> relu -> BN -> MaxPool)
+# And 3 Fully-connected layers
+def vgg(input_shape, dropout, use_bn):
+    model = Sequential()
+
+    model.add(Conv2D(64, 3, 3, activation="relu", input_shape=input_shape))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Conv2D(64, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(128, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Conv2D(128, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(256, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Conv2D(256, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(512, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Conv2D(512, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(1024, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Conv2D(1024, 3, 3, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(MaxPooling2D())
+
+    model.add(Flatten())
+
+    model.add(Dense(4096, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Dense(1000, activation="relu"))
+    if use_bn:
+        model.add(BatchNormalization())
+
+    model.add(Dropout(dropout))
+
+    model.add(Dense(1))
+
+    return model
+
+
+grid = [{}]
+
+
+# Computes cartesian product of values and grid content
+def add_to_grid(name, values):
+    global grid
+
+    new_grid = []
+    for node in grid:
+        for value in values:
+            node_copy = copy.deepcopy(node)
+            node_copy[name] = value
+            new_grid.append(node_copy)
+
+    grid = new_grid
+
+
+add_to_grid("lr", [0.0001, 0.00001, 0.001])
+add_to_grid("dropout", [1.0, 0.5, 0.7, 0.3])
+add_to_grid("bn", [True, False])
+add_to_grid("dataset", ["original", "hsv"])
+add_to_grid("F", [vgg, alexnet])
+
+best_val_loss = 100
+# Grid node with the least validation loss will be saved in this variable
+best_node = None
+
+for i in range(len(grid)):
+    node = grid[i]
+    node["grid"] = i + 1
+
+    print(node)
+
+    lr = node["lr"]
+    dropout = node["dropout"]
+    bn = node["bn"]
+    dataset_name = node["dataset"]
+    F = node["F"]
+
+    dataset = dataset_provider.get(dataset_name)
+    dataset_shape = dataset_provider.get_shape(dataset_name)
+
+    model = F(dataset_shape, dropout, bn)
+
+    model.compile(loss='mse',
+                  optimizer='adam',
+                  lr=lr)
+
+    t0 = time.time()
+
+    model.fit(dataset.X_train, dataset.y_train,
+              batch_size=10, nb_epoch=1,
+              verbose=0)
+
+    val_loss = model.evaluate(dataset.X_validation, dataset.y_validation, verbose=0)
+
+    elapsed_time = time.time() - t0
+
+    node["val_loss"] = val_loss
+    node["elapsed_time"] = elapsed_time
+
+    print("val_loss", val_loss)
+    print("elapsed time:", elapsed_time)
+    print()
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        best_node = node
+        save_model(model, dataset_name, "model")
+
+    # Clear session to avoid OOM in very big grids
+    kb.clear_session()
+
+print("Best node:", best_node)
