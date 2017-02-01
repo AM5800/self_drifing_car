82bbb54afcb883efa3a1b8754877e7861807a743
AM5800 <am5800@yandex.ru>
2017-02-01 20:15:58 +0100

    Extract __create_object_points method

--- advanced_lanes_detection/camera_cal/calibration.py
+++ advanced_lanes_detection/camera_cal/calibration.py
@@ -8,38 +8,43 @@ import os
 
 
 class ChessboardCalibrator:
-    def __init__(self, image_size=None, pattern_size=None, file_name=None):
-        if image_size is not None and pattern_size is not None:
+    def __init__(self, image_size=None, file_name=None):
+        self.__image_points = []
+        self.__object_points = []
+        if image_size is not None:
             self.__image_size = image_size
-            self.__pattern_size = pattern_size
-            self.__image_points = []
-            self.__object_points = []
-            w = pattern_size[0]
-            h = pattern_size[1]
-            self.__object_points_template = np.zeros((w * h, 3), np.float32)
-            self.__object_points_template[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2)
             self.__mtx = None
             self.__dist = None
         elif file_name is not None:
-            dic = pickle.load(open(file_name, "rb"))
-            self.__image_size = dic["image_size"]
-            self.__mtx = dic["mtx"]
-            self.__dist = dic["dist"]
+            data = pickle.load(open(file_name, "rb"))
+            self.__image_size = data["image_size"]
+            self.__mtx = data["mtx"]
+            self.__dist = data["dist"]
+        else:
+            raise Exception("Either image_size or file_name should be filled")
 
-    def add_chessboard_image(self, img):
+    def add_chessboard_image(self, img, pattern_size):
         if img.shape != self.__image_size:
             raise Exception("Image size mismatch!")
 
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, self.__pattern_size, None)
+        ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
         if not ret:
             return False
 
         self.__image_points.append(corners)
-        self.__object_points.append(self.__object_points_template)
+        self.__object_points.append(self.__create_object_points(pattern_size))
         self.__mtx = None
         self.__dist = None
         return True
+
+    @staticmethod
+    def __create_object_points(pattern_size):
+        w = pattern_size[0]
+        h = pattern_size[1]
+        object_points = np.zeros((w * h, 3), np.float32)
+        object_points[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2)
+        return object_points
 
     def save(self, file_name):
         self.__ensure_calibrated()
@@ -70,13 +75,13 @@ if __name__ == "__main__":
         image_size = (720, 1280, 3)
         out_file = "../calibration.p"
         if not os.path.exists(out_file):
-            calibrator = ChessboardCalibrator(image_size, (9, 6))
+            calibrator = ChessboardCalibrator(image_size)
             for img_file in glob.glob("calibration*.jpg"):
                 img = mpimg.imread(img_file)
-                # Crop image to make them all of same size
+                # Crop image to make them all of the same size
                 img = img[0:image_size[0], 0:image_size[1], 0:image_size[2]]
 
-                if not calibrator.add_chessboard_image(img):
+                if not calibrator.add_chessboard_image(img, (9, 6)):
                     print("Chessboard pattern not found in", img_file)
 
             calibrator.save(out_file)
@@ -90,6 +95,5 @@ if __name__ == "__main__":
         result = np.concatenate([src, dst], axis=1)
         plt.imshow(result)
         plt.show()
-
 
     main()
